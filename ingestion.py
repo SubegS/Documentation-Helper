325a1fb4955743b4df5ae7eda5a6d96e929ace89
SubegS <subegsinghkapoor360@gmail.com>
2025-12-14 14:15:03 +0530

    File set up

---
+++ ingestion.py
@@ -0,0 +1,10 @@
+import asyncio
+import os 
+from dotenv import load_dotenv
+load_dotenv()
+
+async def main():
+    """Main async function to orchestrate the entire process."""
+
+if __name__ == "__main__":
+    asyncio.run(main())
