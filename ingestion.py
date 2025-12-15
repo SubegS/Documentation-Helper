7ff994f1a55abfe3b314a66bce0211e494776424
SubegS <subegsinghkapoor360@gmail.com>
2025-12-15 22:45:54 +0530

    Added Embeddign and ssl

--- ingestion.py
+++ ingestion.py
@@ -1,7 +1,26 @@
 import asyncio
 import os 
+import ssl
+from typing import Any, Dict, List
+import certifi
 from dotenv import load_dotenv
+from langchain_openai import OpenAIEmbeddings
+
 load_dotenv()
+
+
+# Configure SSL context to use certifi certificates
+ssl_context = ssl.create_default_context(cafile=certifi.where())
+os.environ["SSL_CERT_FILE"] = certifi.where()
+os.environ["REQUESTS_CA_BUNDLE"] = certifi.where()
+
+embeddings = OpenAIEmbeddings(
+    model="text-embedding-3-small",
+    show_progress_bar=False,
+    chunk_size=50,
+    retry_min_seconds=10,
+)
+
 
 async def main():
     """Main async function to orchestrate the entire process."""
